ae4b5714f33cd8b50af910c6e47000a6120f4ee0
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2016-09-14 13:01:57 +0200

    get rid of 'arch_specific_syscall' dummy and get arm_sync_file_range back

--- update-list.sh
+++ update-list.sh
@@ -6,7 +6,7 @@ grep "#define __NR_" /usr/include/asm-generic/unistd.h /usr/include/asm/unistd.h
         grep -v NR3264 | \
         grep -v __NR_syscall | \
         egrep -v "SYSCALL_BASE" | \
-        grep -v arm_sync_file_range  | \
+        grep -v arch_specific_syscall  | \
         cut -d" " -f2 | \
         sed -e "s/__NR_//g" >$TMP
 cat syscall-names.text >>$TMP
