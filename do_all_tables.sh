ae55351f02725de4172448c1e904672fac21c7df
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2019-05-12 10:48:10 +0200

    parse all asm/unistd*.h headers
    Several architectures have asm/unistd.h split into variants. Use them to
    gather all syscall names.

--- do_all_tables.sh
+++ do_all_tables.sh
@@ -8,7 +8,7 @@ export_headers()
 {
 	make -s -C ${KERNELSRC} ARCH=${arch} O=${PWD}/headers headers_install
 
-	egrep -h "^#define __NR_" ${PWD}/headers/usr/include/asm/unistd.h ${PWD}/headers/usr/include/asm-generic/unistd.h |
+	egrep -h "^#define __NR_" ${PWD}/headers/usr/include/asm/unistd*.h ${PWD}/headers/usr/include/asm-generic/unistd.h |
 		egrep -v "(unistd.h|NR3264|__NR_syscall|__SC_COMP|__NR_.*Linux|__NR_FAST)" |
 		egrep -vi "(not implemented|available|unused|reserved|xtensa|spill)" |
 		egrep -v "(__SYSCALL|SYSCALL_BASE)" |
