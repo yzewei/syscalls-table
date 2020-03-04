7ac2a8bb3e4efe84815daf142e0fbae388768d6e
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2020-03-04 18:54:40 +0100

    drop syscalls from "man 2 unimplemented"

--- do_all_tables.sh
+++ do_all_tables.sh
@@ -4,6 +4,8 @@ KERNELSRC=$1
 
 TMP=`mktemp`
 
+DEAD_SYSCALLS="afs_syscall|break|ftime|gtty|lock|mpx|oldwait4|prof|profil|putpmsg|security|stty|tuxcall|ulimit|vserver"
+
 export_headers()
 {
 	make -s -C ${KERNELSRC} ARCH=${arch} O=${PWD}/headers headers_install &>/dev/null
@@ -11,7 +13,7 @@ export_headers()
 
 	egrep -h "^#define __NR_" ${PWD}/headers/usr/include/asm/unistd*.h ${PWD}/headers/usr/include/asm-generic/unistd.h |
 		egrep -v "(unistd.h|NR3264|__NR_syscall|__SC_COMP|__NR_.*Linux|__NR_FAST)" |
-		egrep -vi "(not implemented|available|unused|reserved|xtensa|spill)" |
+		egrep -vi "(not implemented|available|unused|reserved|xtensa|spill|${DEAD_SYSCALLS})" |
 		egrep -v "(__SYSCALL|SYSCALL_BASE)" |
 		sed -e "s/#define\s*__NR_//g" -e "s/\s.*//g" |
 		sort -u >${TMP}
--- grab_syscall_names_from_tables.sh
+++ grab_syscall_names_from_tables.sh
@@ -2,9 +2,11 @@
 
 TMP=`mktemp`
 
+DEAD_SYSCALLS="afs_syscall|break|ftime|gtty|lock|mpx|oldwait4|prof|profil|putpmsg|security|stty|tuxcall|ulimit|vserver"
+
 for tbl_file in $(find $KERNELSRC -name syscall*.tbl)
 do
-	egrep -v "(^#|^$|sys_ni_syscall|unused)" $tbl_file |
+	egrep -v "(^#|^$|sys_ni_syscall|unused|${DEAD_SYSCALLS})" $tbl_file |
 		awk '{ print $3 }' > ${TMP}
 	cat syscall-names.text >>${TMP}
 	LC_ALL=C sort -u ${TMP} >syscall-names.text
