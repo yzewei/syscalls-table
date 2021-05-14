4d67767e9f5d6ca2892d09874120576cc554b041
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2021-05-14 08:09:29 +0200

    some SEO related changes
    - manpages links got title
    - meta description/keywords are present
    - twitter card + opengraph tags

--- parser.py
+++ parser.py
@@ -111,8 +111,8 @@ def generate_html_file():
 
         syscall_table += f"""
     <tr class='{trclass}'>
-    <td><a href='https://www.man7.org/linux/man-pages/man2/{syscall}.2.html'>
-    {syscall}</a></td>"""
+    <td><a href='https://www.man7.org/linux/man-pages/man2/{syscall}.2.html'
+     title='{syscall} manpage' > {syscall}</a></td>"""
 
         for arch in archs:
 
