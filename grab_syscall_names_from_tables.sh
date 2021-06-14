7eb8decd999d01440e1203ff4d6bb80776a8c7c1
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2021-06-14 13:00:12 +0200

    we want only kernel lists
    tools/perf/ ones are not needed

--- grab_syscall_names_from_tables.sh
+++ grab_syscall_names_from_tables.sh
@@ -8,7 +8,7 @@ TMP=`mktemp`
 #
 DEAD_SYSCALLS="afs_syscall|break|ftime|gtty|lock|mpx|oldwait4|prof|profil|putpmsg|security|stty|tuxcall|ulimit|vserver|arm_sync_file_range|utimesat|ni_syscall"
 
-for tbl_file in $(find $KERNELSRC -name syscall*.tbl)
+for tbl_file in $(find ${KERNELSRC}/arch -name syscall*.tbl)
 do
 	egrep -v "(^#|^$|sys_ni_syscall|unused|${DEAD_SYSCALLS})" $tbl_file |
 		awk '{ print $3 }' > ${TMP}
