12120e52e86f54cbd8171e1a139fe0965e29c672
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2022-02-14 20:34:21 +0100

    parser: use Jinja2 for template
    I use Jinja2 in other projects so having unified templates helps.

--- parser.py
+++ parser.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-import csv
+from csv import reader
 from datetime import datetime
-import io
-import os
-import sys
-
-from string import Template
+from jinja2 import Environment, FileSystemLoader
+from os import listdir
+from sys import argv
 
 
 def load_syscall_data():
@@ -15,20 +13,19 @@ def load_syscall_data():
     present_archs = []
 
     # use current system call names - tables/* can contain archive data
-    with io.open("syscall-names.text", newline="") as csvh:
-        syscalldata = csv.reader(csvh, delimiter="\t")
+    with open("syscall-names.text", newline="") as csvh:
+        syscalldata = reader(csvh, delimiter="\t")
 
         for row in syscalldata:
             syscalls[row[0]] = {}
 
-    os.chdir("tables")
-    for filename in os.listdir(os.getcwd()):
+    for filename in listdir("tables"):
 
         try:
             arch = filename.replace("syscalls-", "")
 
-            with io.open(filename, newline="") as csvh:
-                syscalldata = csv.reader(csvh, delimiter="\t")
+            with open(f"tables/{filename}", newline="") as csvh:
+                syscalldata = reader(csvh, delimiter="\t")
                 present_archs.append(arch)
 
                 for row in syscalldata:
@@ -91,66 +88,28 @@ def create_arch_list(present_archs):
 
 
 def generate_html_file():
-    archs_header = ""
 
-    for arch in archs:
-        archs_header += f"<th>{arch}</th>"
+    file_loader = FileSystemLoader('templates')
+    env = Environment(loader=file_loader, trim_blocks=True, lstrip_blocks=True)
 
-    syscall_table = ""
-    oddeven = 0
+    template = env.get_template('syscalls.html.j2')
 
-    for syscall in sorted(syscalls.keys()):
-
-        if oddeven % 2 == 0:
-            trclass = "even"
-        else:
-            trclass = "odd"
-
-        oddeven += 1
-
-        syscall_table += f"""
-    <tr class='{trclass}'>
-    <td><a href='https://www.man7.org/linux/man-pages/man2/{syscall}.2.html'
-     title='{syscall} manpage' > {syscall}</a></td>"""
-
-        for arch in archs:
-
-            try:
-                syscallnr = syscalls[syscall][arch]
-                css = ""
-            except KeyError:
-                syscallnr = -1
-                css = ' class="legacy" '
-
-            if not syscallnr:
-                syscallnr = -1
-                css = ' class="legacy" '
-
-            syscall_table += f"<td {css}>{syscallnr}</td>"
-
-        syscall_table += "</tr>"
-
-    with open("../template.html") as html_file:
-        html = Template(html_file.read())
-
-        print(
-            html.substitute(
-                generate_time=datetime.strftime(
-                    datetime.utcnow(), "%Y.%m.%d %H:%M"
-                ),
-                kernel_version=kernel_version,
-                archs_header=archs_header,
-                syscall_table=syscall_table,
-            )
-        )
+    output = template.render(generate_time=datetime.strftime(
+                             datetime.utcnow(), "%d %B %Y %H:%M"),
+                             archs=archs,
+                             kernel_version=kernel_version,
+                             syscall_names=sorted(syscalls.keys()),
+                             syscall_data=syscalls,
+                             minify=True)
+    print(output)
 
 
 if __name__ == "__main__":
 
     kernel_version = ""
 
-    if len(sys.argv) > 1:
-        kernel_version = sys.argv[1]
+    if len(argv) > 1:
+        kernel_version = argv[1]
 
     [syscalls, present_archs] = load_syscall_data()
     archs = create_arch_list(present_archs)
