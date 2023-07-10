32d8b6ffe8064df0c12489e84535be62e2cc8900
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-07-10 11:50:25 +0200

    Handle 32-bit architectures as 32-bit
    Linux 6.5-rc1 gave wrong output for those 32-bit architectures which do
    not have own syscalls.tbl files but use default system call numbers.

--- do_all_tables.sh
+++ do_all_tables.sh
@@ -27,6 +27,10 @@ generate_table()
 {
 	echo -n "$arch "
 
+	if [ $bits == 32 ]; then
+		extraflags="${extraflags} -D__BITS_PER_LONG=32"
+	fi
+
 	gcc list-syscalls.c -U__LP64__ -U__ILP32__ -U__i386__ -D${arch^^} \
 		-D__${arch}__ ${extraflags} -I headers/usr/include/ -o list-syscalls &>/dev/null
 	./list-syscalls > "tables/syscalls-$arch"
@@ -36,6 +40,9 @@ generate_table()
 for archdir in $KERNELSRC/arch/*
 do 
 	arch=`basename $archdir`
+
+	bits=64
+	extraflags=
 
 	case ${arch} in
 	Kconfig)
@@ -50,6 +57,7 @@ do
 
 	case ${arch} in
 	arm)
+		bits=32
 		arch=armoabi		extraflags=				generate_table
 		arch=arm		extraflags=-D__ARM_EABI__		generate_table
 		;;
@@ -58,24 +66,30 @@ do
 		arch=loongarch64	extraflags=-D_LOONGARCH_SZLONG=64	generate_table
 		;;
 	mips)
+		arch=mips64		extraflags=-D_MIPS_SIM=_MIPS_SIM_ABI64	generate_table
+		bits=32
 		arch=mipso32		extraflags=-D_MIPS_SIM=_MIPS_SIM_ABI32	generate_table
 		arch=mips64n32		extraflags=-D_MIPS_SIM=_MIPS_SIM_NABI32	generate_table
-		arch=mips64		extraflags=-D_MIPS_SIM=_MIPS_SIM_ABI64	generate_table
 		;;
 	powerpc)
 										generate_table
 		arch=powerpc64							generate_table
 		;;
 	riscv)
-		arch=riscv32		extraflags=-D__SIZEOF_POINTER__=4	generate_table
 		arch=riscv64		extraflags=-D__LP64__			generate_table
+		bits=32
+		arch=riscv32		extraflags=-D__SIZEOF_POINTER__=4	generate_table
 		;;
 	s390)
+		bits=32
 										generate_table
+		bits=64
 		arch=s390x							generate_table
 		;;
 	sparc)
+		bits=32
 					extraflags=-D__32bit_syscall_numbers__	generate_table
+		bits=64
 		arch=sparc64		extraflags=-D__arch64__			generate_table
 		;;
 	tile)
@@ -83,9 +97,13 @@ do
 		arch=tile64		extraflags="-D__LP64__ -D__tilegx__"	generate_table
 		;;
 	x86)
+		arch=x86_64		extraflags=-D__LP64__			generate_table
+		bits=32
 		arch=i386							generate_table
 		arch=x32		extraflags=-D__ILP32__			generate_table
-		arch=x86_64		extraflags=-D__LP64__			generate_table
+		;;
+	arc|csky|hexagon|m68k|microblaze|nios2|openrisc|sh|xtensa)
+		bits=32 							generate_table
 		;;
 	*)
 		generate_table
