7e0dd3cb433f66c70364c5c574b085f68d3ffdb4
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-11-02 17:00:38 +0100

    handle removed system calls on update
    There are 89 not implemented system calls present in Linux kernel.
    Make sure they do not show again.
    
    data/removed-names.text lists entries we do not want to see.

--- scripts/update-tables.sh
+++ scripts/update-tables.sh
@@ -4,6 +4,9 @@ DATADIR=$PWD/data
 PYTHONDIR=$PWD
 KERNELSRC=$1
 
+# to keep sorting in order
+export LC_ALL=C
+
 if [ -z $KERNELSRC ]; then
 	echo "give me path to Linux kernel sources:"
 	echo ""
@@ -22,20 +25,45 @@ TEMP=$(mktemp -d)
 
 grab_syscall_names_from_tables()
 {
-	# arm_sync_file_range is sync_file_range2() since 2.6.22
-	# utimesat is PowerPC/SPU only
-	# rest are not implemented
-	#
-	DEAD_SYSCALLS="afs_syscall|break|ftime|gtty|lock|mpx|oldwait4|prof|profil|putpmsg|security|stty|tuxcall|ulimit|vserver|arm_sync_file_range|utimesat|ni_syscall"
-	FAKE_ENTRIES="^(available|reserved|unused|SYSCALL_MASK).*$"
-
 	for tbl_file in $(find ${KERNELSRC}/arch -name syscall*.tbl)
 	do
-		grep -E -v "(^#|^$|sys_ni_syscall)" $tbl_file | awk '{ print $3 }' >> ${TEMP}/syscall-names.text
+		grep -E -v "(^#|^$|sys_ni_syscall)" $tbl_file | awk '{ print $3 }' >> ${TEMP}/syscall-names.tosort
 	done
 
+	drop_bad_entries
+}
+
+grab_syscall_names_from_unistd_h()
+{
+	grep -E -h "^#define __NR_" ${PWD}/headers/usr/include/asm/unistd*.h \
+		                    ${PWD}/headers/usr/include/asm-generic/unistd.h > ${TEMP}/syscall-names.tosort
+
+	drop_bad_entries
+}
+
+drop_bad_entries()
+{
+	grep -E -v "(unistd.h|NR3264|__NR_syscall|__SC_COMP|__NR_.*Linux|__NR_FAST)"  ${TEMP}/syscall-names.tosort |
+	grep -E -vi "(not implemented|available|unused|reserved|xtensa|spill)" |
+	grep -E -v "(__SYSCALL|SYSCALL_BASE|SYSCALL_MASK)" |
+	sed -e "s/#define\s*__NR_//g" -e "s/\s.*//g" |
+	sort -u >${TEMP}/syscall-names.text
 	cat ${DATADIR}/syscall-names.text >>${TEMP}/syscall-names.text
-	LC_ALL=C sort -u  ${TEMP}/syscall-names.text | grep -E -v $FAKE_ENTRIES | grep -E -v -w $DEAD_SYSCALLS >${DATADIR}/syscall-names.text
+	grep -w -v -f ${DATADIR}/removed-names.text ${TEMP}/syscall-names.text | sort -u >${DATADIR}/syscall-names.text
+}
+
+generate_table()
+{
+	echo -n "$arch "
+	echo $arch >> ${DATADIR}/architectures-present-in-kernel.text
+
+	if [ $bits == 32 ]; then
+		extraflags="${extraflags} -D__BITS_PER_LONG=32"
+	fi
+
+	gcc list-syscalls.c -U__LP64__ -U__ILP32__ -U__i386__ -D${arch^^} \
+		-D__${arch}__ ${extraflags} -I headers/usr/include/ -o list-syscalls &>/dev/null
+	./list-syscalls > "${DATADIR}/tables/syscalls-$arch"
 }
 
 generate_list_syscalls_c()
@@ -48,7 +76,7 @@ generate_list_syscalls_c()
 	{
 	"
 
-	for syscall in `cat data/syscall-names.text`
+	for syscall in `cat ${DATADIR}/syscall-names.text`
 	do
 		echo "
 	#ifdef __NR_$syscall
@@ -67,34 +95,6 @@ generate_list_syscalls_c()
 export_headers()
 {
 	make -s -C ${KERNELSRC} ARCH=${arch} O=${PWD}/headers headers_install &>/dev/null
-}
-
-grab_syscall_names_from_unistd_h()
-{
-	grep -E -h "^#define __NR_" ${PWD}/headers/usr/include/asm/unistd*.h ${PWD}/headers/usr/include/asm-generic/unistd.h |
-		grep -E -v "(unistd.h|NR3264|__NR_syscall|__SC_COMP|__NR_.*Linux|__NR_FAST)" |
-		grep -E -vi "(not implemented|available|unused|reserved|xtensa|spill)" |
-		grep -E -v "(__SYSCALL|SYSCALL_BASE|SYSCALL_MASK)" |
-		sed -e "s/#define\s*__NR_//g" -e "s/\s.*//g" |
-		grep -E -v -w $DEAD_SYSCALLS |
-		sort -u >${TEMP}/syscall-names.text
-	cat ${DATADIR}/syscall-names.text >>${TEMP}/syscall-names.text
-	sed -i '/^arch_specific_syscall$/d' ${TEMP}/syscall-names.text
-	LC_ALL=C sort -u ${TEMP}/syscall-names.text >${DATADIR}/syscall-names.text
-}
-
-generate_table() 
-{
-	echo -n "$arch "
-	echo $arch >> ${DATADIR}/architectures-present-in-kernel.text
-
-	if [ $bits == 32 ]; then
-		extraflags="${extraflags} -D__BITS_PER_LONG=32"
-	fi
-
-	gcc list-syscalls.c -U__LP64__ -U__ILP32__ -U__i386__ -D${arch^^} \
-		-D__${arch}__ ${extraflags} -I headers/usr/include/ -o list-syscalls &>/dev/null
-	./list-syscalls > "${DATADIR}/tables/syscalls-$arch"
 }
 
 do_all_tables()
@@ -214,7 +214,6 @@ create_tables_for_python()
 	echo "linux_version = \"${KVER}\"" > ${PYTHONDIR}/system_calls/linux_version.py
 }
 
-touch data/syscall-names.text
 grab_syscall_names_from_tables
 generate_list_syscalls_c | sed -e "s/'/\"/g">${TEMP}/list-syscalls.c
 
